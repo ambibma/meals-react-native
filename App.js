ffacc941ecdfe7bb1bec82bef532c7ca1252596e
ambi hidalgo <ambio.pk@gmail.com>
2023-07-11 16:36:55 -0700

    Add stack navigation between screens

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import CategoriesScreen from './screens/CategoriesScreen'
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
+import MealDetails from './screens/MealDetailsScreen';
+import MealDetailsScreen from './screens/MealDetailsScreen';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -13,10 +16,31 @@ export default function App() {
     <StatusBar style="light"/>
 
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{
+      
+          headerStyle: {backgroundColor: '#ff8243'},
+          headerTintColor: 'white',
+          contentStyle: {backgroundColor: '#5b2525'}
+        }}>
         {/* Name of the screen, component serves the screen, not JSX just a pointer at the component function */}
-        <Stack.Screen name="MealsCategories" component={CategoriesScreen}/>
-        <Stack.Screen name='MealsOverview' component={MealsOverviewScreen}/>
+        <Stack.Screen name="MealsCategories" 
+        component={CategoriesScreen}
+        options={{
+          title: 'All Categories'
+        }}
+        />
+        <Stack.Screen name='MealsOverview' 
+        component={MealsOverviewScreen}
+        // options={({route, navigation}) => {
+        //   const catId = route.params.categoryId;
+        //   return {
+        //     title: catId,
+        //   }
+        // }}
+        />
+        <Stack.Screen name='MealDetailsScreen'
+        component={MealDetailsScreen}        
+        />
       </Stack.Navigator>
     </NavigationContainer>
     

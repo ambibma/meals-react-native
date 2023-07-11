ae80efe1ac209fd8618763b953aa622a7f8c7eb9
ambi hidalgo <ambio.pk@gmail.com>
2023-07-11 16:39:31 -0700

    Add navigation route, and meals screen component

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,10 +1,60 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { MEALS } from '../data/dummy-data'
+import { StyleSheet, Text, View, FlatList, Pressable } from 
+'react-native'
+import { useLayoutEffect } from 'react';
+import { MEALS, CATEGORIES} from '../data/dummy-data'
+import { useRoute } from '@react-navigation/native';
+import Meal from '../models/meal';
+import MealItem from '../components/MealItem';
 
-export default function MealsOverviewScreen() {
+export default function MealsOverviewScreen({route, navigation}) {
+  const catId = route.params.categoryId;
+  const mealId = route.params.mealsId;
+  
+  const displayedMeals = MEALS.filter((mealItem)=> {
+    return mealItem.categoryIds.indexOf(catId ) >= 0;
+  });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find((category) => category.id ===catId).title;
+  
+    navigation.setOptions({
+        title: categoryTitle
+    })
+    
+    
+  }, [catId, navigation])
+
+
+  function renderMealItem(itemData) {
+    // function pressHandler(){
+    //   navigation.navigate('MealDetails',{
+    //    mealId: itemData.id,
+    //   })
+    
+    const item = itemData.item;
+    const mealItemProps = {
+      id: item.id,
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+    return <MealItem {...mealItemProps}/>
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen</Text>
+  
+      <FlatList data={displayedMeals}
+      keyExtractor={(item) => item.id}
+      renderItem={renderMealItem} />
+
+ 
+
+
+  
+      <Text>Meals Overview Screen - {catId}</Text>
     </View>
   )
 }
